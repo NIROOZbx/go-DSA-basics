3d973ab55fe1b80ef674bbc04349e3d863cc8268
NIROOZbx <nyroozashraf@gmail.com>
2025-10-20 11:45:39 +0530

    "queue operations"

---
+++ task8/Queue/queueops.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Queue struct {
+	items       [5]int
+	currentSize int
+	Start       int
+	End         int
+}
+
+func (q *Queue) Enqueue(val int) {
+
+	if q.currentSize == len(q.items) {
+		fmt.Println("Queue limit exceeded")
+		return
+	}
+
+
+	q.items[q.End] = val
+	q.End = (q.End + 1) % len(q.items)
+	q.currentSize++
+
+}
+
+func (q *Queue) Dequeue() {
+	
+	if q.currentSize == 0 {
+		return 
+	}
+
+	q.Start=(q.Start+1)%len(q.items)
+	q.currentSize--
+	fmt.Println(q.Start)
+}
+
+func (q *Queue) Peek() int {
+	return q.items[q.Start]
+}
+
+func main() {
+	var Q Queue
+
+	Q.Enqueue(10)
+	Q.Enqueue(20)
+	Q.Enqueue(30)
+	Q.Enqueue(40)
+	Q.Enqueue(50)
+	Q.Dequeue()
+	Q.Enqueue(60)
+	
+
+	fmt.Println("Item at start position", Q.Peek())
+	fmt.Println("Item at end position", Q.items[Q.End])
+	fmt.Println(Q.items)
+}
--- task8/Stack/stackops.go
+++ task8/Stack/stackops.go
@@ -10,20 +10,18 @@ var top = -1
 
 func (s *Stack) Push(val int) {
 	top++
-	
-
 	s.items = append(s.items, val)
 
 }
 
-func(s *Stack) Pop() []int{
-	
+func (s *Stack) Pop() []int {
+
 	top--
 
 	return s.items[:top+1]
 }
 
-func (s *Stack) Peek()int{
+func (s *Stack) Peek() int {
 	return s.items[top]
 }
 
@@ -31,11 +29,10 @@ func main() {
 
 	var st Stack
 
-	st.Push(5)
 	st.Push(10)
-	st.Push(50)
-
-	
+	st.Push(20)
+	st.Push(30)
+	st.Push(40)
 
 	fmt.Println(st.Pop())
 	fmt.Println(st.Peek())
